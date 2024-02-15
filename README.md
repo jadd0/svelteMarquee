841626fd036356262d492025c9ffe87fe3183b20
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-02-15 21:10:09 +0000

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+## Svelte Marquee
+
+This package allows you to create a simple marquee in Svelte
+
+## How to use:
+
+Download the package using:
+
+```terminal
+npm i sveltemarquee
+```
+
+Then import it into your project with:
+
+```javascript
+import { SvelteMarquee } from 'sveltemarquee';
+```
+
+Then use it with:
+
+```svelte
+<SvelteMarquee>
+  <h1>Hello World!</h1>
+</SvelteMarquee>
+```
+
+The speed is automatically set to 50 seconds, but it can be changed by passing a value into the prop:
+
+```svelte
+<SvelteMarquee speed={20}>
+  <h1>Hello World!</h1>
+</SvelteMarquee>
+```
+
+Thank you for using my package! :)
---
+++ src/lib/SvelteMarquee.svelte
@@ -0,0 +1,71 @@
+<script>
+  export let speed = 50;
+</script>
+
+<div class="marquee">
+  <ul
+    class="marquee__content"
+    style="animation: scroll {speed}s linear infinite !important; color: rgb(82, 82, 82) !important"
+  >
+    <slot></slot>
+  </ul>
+  <ul
+    aria-hidden="true"
+    class="marquee__content"
+    style="animation: scroll {speed}s linear infinite !important; color: rgb(82, 82, 82) !important"
+  >
+    <slot></slot>
+  </ul>
+</div>
+
+
+<style>
+  .marquee {
+		--gap: 1rem;
+		position: relative;
+		display: flex;
+		overflow: hidden;
+		user-select: none;
+    margin: none;
+	}
+
+	.marquee__content {
+		flex-shrink: 0;
+		display: flex;
+		justify-content: space-around;
+		min-width: 100%;
+	}
+
+	@keyframes -global-scroll {
+		from {
+			transform: translateX(0);
+		}
+		to {
+			transform: translateX(calc(-100% - var(--gap)));
+		}
+	}
+
+	@media (prefers-reduced-motion: reduce) {
+		.marquee__content {
+			animation-play-state: paused !important;
+		}
+	}
+
+	@keyframes -global-scroll-abs {
+		from {
+			transform: translateX(calc(100% + var(--gap)));
+		}
+		to {
+			transform: translateX(0);
+		}
+	}
+
+	.marquee__content:last-child {
+		animation-name: scroll-abs;
+	}
+
+	ul {
+		list-style-type: none;
+		font-size: 40px;
+	}
+</style>
